a056706225ae4ba7059fb96f6c2f7b2e47266053
jasieksz <jasiek.sznajd@gmail.com>
2018-04-21 14:17:17 +0200

    [KILL-27] GameState builder pattern

--- kilines/src/main/java/model/GameState.java
+++ kilines/src/main/java/model/GameState.java
@@ -4,8 +4,11 @@ import service.CollisionDetectionService;
 import service.UpdateDirectionService;
 import service.UpdatePositionService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class GameState {
     private Map<Point, PlayerIdentifier> board;
@@ -55,5 +58,76 @@ public class GameState {
                 .filter(Motorcycle::isAlive)
                 .filter(motorcycle -> motorcycle.getPlayerId().getPlayerId() == playerId)
                 .forEach(motorcycle -> updateDirectionService.updateDirection(motorcycle, direction));
+    }
+
+    /*
+     * GAME STATE INITIALIZER
+     */
+    public static class InitializerBuilder {
+        private final int minPlayers;
+        private int x;
+        private int y;
+
+        private final PlayerIdentifier wall = new PlayerIdentifier(Color.WHITE, 0);
+        private Map<Point, PlayerIdentifier> board = new HashMap<>();
+        private List<Motorcycle> motorcycleList = new ArrayList<>();
+
+        public InitializerBuilder(int mp) {
+            minPlayers = mp;
+        }
+
+        public InitializerBuilder boardX(int x) {
+            this.x = x;
+            return this;
+        }
+
+        public InitializerBuilder boardY(int y) {
+            this.y = y;
+            return this;
+        }
+
+        public InitializerBuilder addPlayer(Point initialPosition, PlayerIdentifier id) {
+            motorcycleList.add(new Motorcycle(id, initialPosition));
+            return this;
+        }
+
+        public InitializerBuilder addWalls() {
+            createWalls();
+            return this;
+        }
+
+        public InitializerBuilder addAGHWalls(){
+            createAGHThemeWalls();
+            return this;
+        }
+
+        public GameState build() {
+            while (motorcycleList.size() < minPlayers){
+                addBot(new Point(0,0), wall); // TODO: Prepare for robot uprising
+            }
+            return new GameState(this.board, this.motorcycleList);
+        }
+
+
+        private void addBot(Point initialPosition, PlayerIdentifier id) {
+            motorcycleList.add(new Motorcycle(id, initialPosition));
+        }
+
+        private void createWalls() {
+            IntStream.range(0, x).forEach(i -> {
+                board.put(new Point(i, 0), wall);
+                board.put(new Point(i, y - 1), wall);
+            });
+            IntStream.range(0, y).forEach(i -> {
+                board.put(new Point(0, i), wall);
+                board.put(new Point(x - 1, i), wall);
+            });
+        }
+
+        private void createAGHThemeWalls(){
+            return;
+        }
+
+
     }
 }
--- kilines/src/main/java/server/Server.java
+++ kilines/src/main/java/server/Server.java
@@ -1,24 +1,28 @@
 package server;
 
+import model.Color;
 import model.GameState;
+import model.PlayerIdentifier;
+import model.Point;
 import rx.Observable;
 
 import java.util.concurrent.TimeUnit;
 
 public class Server {
-    private static GameState gameState;
     public static void main(String[] args) throws Exception {
 
-         gameState = new Initializer(GameUtils.boardX, GameUtils.boardy).initializeGameState();
+         GameState gameState = new GameState.InitializerBuilder(1)
+                .boardX(640)
+                .boardY(480)
+                .addWalls()
+                .addAGHWalls()
+                .addPlayer(new Point(100,100), new PlayerIdentifier(Color.RED, 1))
+                .build();
 
         Observable.interval(GameUtils.interval, TimeUnit.MICROSECONDS)
                 .toBlocking()
                 .subscribe(tick -> {
                     gameState.movePlayers();
                 });
-    }
-
-    public GameState getGameState(){
-        return gameState;
     }
 }
