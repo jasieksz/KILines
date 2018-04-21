4ec2cea761c4f60615855c774a6565e999b1a915
Vladyslav Lopatin <ilopatin3@gmail.com>
2018-04-21 11:49:06 +0200

    service init

--- kilines/src/main/java/server/Server.java
+++ kilines/src/main/java/server/Server.java
@@ -1,7 +1,18 @@
 package server;
 
+import model.GameState;
+
+import java.util.Map;
+
 public class Server {
+
+    private GameState gameState = new GameState(null, null);
+
     public static void main(String[] args) {
         System.out.println("");
+    }
+
+    public GameState getGameState() {
+        return gameState;
     }
 }
