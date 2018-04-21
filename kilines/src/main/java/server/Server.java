4b65e796394aa83d6e5d7f756202db51b57325c2
jasieksz <jasiek.sznajd@gmail.com>
2018-04-21 14:17:17 +0200

    [KILL-27] GameState builder pattern

--- kilines/src/main/java/server/Server.java
+++ kilines/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
+import model.Color;
 import model.GameState;
+import model.PlayerIdentifier;
+import model.Point;
 import rx.Observable;
 
 import java.util.concurrent.TimeUnit;
@@ -8,7 +11,13 @@ import java.util.concurrent.TimeUnit;
 public class Server {
     public static void main(String[] args) throws Exception {
 
-        GameState gameState = new Initializer(GameUtils.boardX, GameUtils.boardy).initializeGameState();
+        GameState gameState = new GameState.InitializerBuilder(1)
+                .boardX(640)
+                .boardY(480)
+                .addWalls()
+                .addAGHWalls()
+                .addPlayer(new Point(100,100), new PlayerIdentifier(Color.RED, 1))
+                .build();
 
         Observable.interval(GameUtils.interval, TimeUnit.MICROSECONDS)
                 .toBlocking()
