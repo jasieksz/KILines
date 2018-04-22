52174afd8bad1733ad4c3474e78f301a427f13d4
Patrycja Tarasińska <patrycja.tarasinska@gmail.com>
2018-04-22 15:22:07 +0200

    push na yolo

--- kilines/src/main/java/service/UpdateDirectionService.java
+++ kilines/src/main/java/service/UpdateDirectionService.java
@@ -5,6 +5,18 @@ import model.Motorcycle;
 
 public class UpdateDirectionService {
     public void updateDirection(Motorcycle motorcycle, Direction direction) {
+        if(direction == Direction.LEFT && motorcycle.getDirection() == Direction.RIGHT)
+            return;
+
+        if(direction == Direction.RIGHT && motorcycle.getDirection() == Direction.LEFT)
+            return;
+
+        if(direction == Direction.UP && motorcycle.getDirection() == Direction.DOWN)
+            return;
+
+        if(direction == Direction.DOWN && motorcycle.getDirection() == Direction.UP)
+            return;
+
         motorcycle.setDirection(direction);
     }
 }
