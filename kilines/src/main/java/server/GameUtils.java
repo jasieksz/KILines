04251ec51b9f727f4fdfdd00ed5f26dca4fd4d07
jasieksz <jasiek.sznajd@gmail.com>
2018-04-22 15:42:42 +0200

    hiher interval

--- kilines/src/main/java/server/GameUtils.java
+++ kilines/src/main/java/server/GameUtils.java
@@ -1,7 +1,7 @@
 package server;
 
 public class GameUtils {
-    public static final int interval = 50;
+    public static final int interval = 100;
     public static final int boardX = 640;
     public static final int boardY = 480;
     public static final String WALL = "wall";
