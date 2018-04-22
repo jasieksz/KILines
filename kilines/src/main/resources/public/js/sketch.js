9243af2f5aed4fd88cc4ff39680fa85b5e3d5867
Bartosz Radzyński <bradzynski@virtuslab.co>
2018-04-22 09:51:32 +0200

    [KILL-39] Adding KI image

--- kilines/src/main/resources/public/js/sketch.js
+++ kilines/src/main/resources/public/js/sketch.js
@@ -1,15 +1,18 @@
 const w = 1280, h = 960;
-const playerStartX = w / 2 - 80;
-const playerStartY = h / 2;
 const backgroundColor = 23;
 const rectW = 4;
 const rectH = 4;
 const playerColours = [
     [0, 167, 247]
 ];
+let deathSound;
 
 let players;
 let communicator = new Communicator(location.href.replace('/game.html',''), handleUpdate);
+
+function preload() {
+    deathSound = loadSound('sound/explosion.mp3');
+}
 
 function setup() {
     createCanvas(w, h);
@@ -24,7 +27,6 @@ function draw() {
 }
 
 function initState(state){
-    console.log(state);
     state.obstacles.map((obs) => {
         renderPoint(obs.pos, state.colors[obs.color]);
     });
@@ -48,14 +50,15 @@ function renderPlayers() {
 
 function scoreboard() {
     if(players && players.length > 0){
-        fill(0);
+        fill(255);
+        stroke(255);
         textFont("Orbitron");
         textSize(18);
-        text("Score");
-        let i = 0;
+        text("Score", 1000, 25);
+        let i = 25;
         players.map((player) =>{
             i += 25;
-            text(player.nick + " : " + player.score, 1000, 25)
+            text(player.nick + " : " + player.score, 1000, i)
             }
         );
        }
@@ -70,16 +73,16 @@ function renderPoint(pos, color) {
 function keyPressed() {
     if (key === 'w' || key === 'W'){
         console.log('w');
-        communicator.update(0);
+        communicator.update("UP");
     } else if (key === 'd' || key === 'D'){
         console.log('d');
-        communicator.update(1);
+        communicator.update("RIGHT");
     } else if (key === 's' || key === 'S'){
         console.log('s');
-        communicator.update(2);
+        communicator.update("DOWN");
     } else if (key === 'a' || key === 'A'){
         console.log('a');
-        communicator.update(3);
+        communicator.update("LEFT");
     }
 }
 
@@ -91,6 +94,7 @@ class Player {
         this.y = data.pos.y;
         this.nick = data.nick;
         this.score = 0;
+        this.isAlive = true;
     }
 
     render () {
@@ -106,52 +110,12 @@ function handleUpdate(update){
             if (selectedPlayer) {
                 selectedPlayer.x = pl.pos.x;
                 selectedPlayer.y = pl.pos.y;
+                if (selectedPlayer.isAlive !== pl.isAlive) kill(selectedPlayer);
             }
         }
     )
 }
 
-let c = 0;
-
-function getState() {
-    return {
-        'type' : 'init',
-        'players': [
-            {
-                'nick': 'Andrzej',
-                'pos': {x: playerStartX, y: playerStartY},
-                'dir': 0
-            }
-        ],
-        'colors': [[244, 66, 66],[143, 244, 65]],
-        'obstacles' :[
-            {'pos': {'x': 0,'y': 0}, 'color': 0},
-            {'pos': {'x': 0,'y': 1}, 'color': 0},
-            {'pos': {'x': 0,'y': 2}, 'color': 0},
-            {'pos': {'x': 0,'y': 3}, 'color': 0},
-            {'pos': {'x': 0,'y': 4}, 'color': 0},
-            {'pos': {'x': 0,'y': 5}, 'color': 0},
-            {'pos': {'x': 0,'y': 6}, 'color': 0},
-            {'pos': {'x': 0,'y': 7}, 'color': 0},
-            {'pos': {'x': 0,'y': 8}, 'color': 0},
-            {'pos': {'x': 0,'y': 9}, 'color': 0},
-            {'pos': {'x': 0,'y': 10}, 'color': 0},
-            {'pos': {'x': 0,'y': 11}, 'color': 0},
-            {'pos': {'x': 0,'y': 12}, 'color': 0},
-            {'pos': {'x': 0,'y': 13}, 'color': 0},
-            {'pos': {'x': 0,'y': 14}, 'color': 0},
-            {'pos': {'x': 0,'y': 15}, 'color': 0},
-            {'pos': {'x': 0,'y': 16}, 'color': 0},
-            {'pos': {'x': 0,'y': 17}, 'color': 0},
-            {'pos': {'x': 300,'y': 300}, 'color': 1},
-            {'pos': {'x': 301,'y': 300}, 'color': 1},
-            {'pos': {'x': 302,'y': 300}, 'color': 1},
-            {'pos': {'x': 303,'y': 300}, 'color': 1},
-            {'pos': {'x': 304,'y': 300}, 'color': 1},
-            {'pos': {'x': 305,'y': 300}, 'color': 1},
-            {'pos': {'x': 306,'y': 300}, 'color': 1},
-            {'pos': {'x': 307,'y': 300}, 'color': 1}
-        ]
-    }
+function kill(player) {
+    deathSound.play();
 }
-
